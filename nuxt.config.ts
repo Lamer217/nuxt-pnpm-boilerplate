5c1c74484e213916978defc2ca6a1863c34315ae
Andrii Malesh <malesh@lawlift.com>
2025-02-23 20:32:55 +0000

    Add flag disabling Nuxt telemetry to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  telemetry: false,
   modules: ['@nuxt/eslint', '@nuxt/fonts'],
   fonts: {
     // Add Google Font Montserrat example
